d2ee75ad54c0250b4007d88241956df4e5a5656e
Matthew Sital-Singh <mattss@netsight.co.uk>
2016-10-19 12:51:23 +0100

    Read config file locally

--- todo.py
+++ todo.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import json
+import os
 
 TRELLO_BOARD_URL = ("https://api.trello.com/1/boards/{boardid}"
                     "?lists=open&list_fields=name&fields=name,desc"
@@ -8,8 +9,11 @@ TRELLO_BOARD_URL = ("https://api.trello.com/1/boards/{boardid}"
 TRELLO_LIST_URL = ("https://api.trello.com/1/lists/{listid}?fields=name"
                    "&cards=open&card_fields=name"
                    "&key={key}&token={token}")
+CONFIG_FILENAME = os.path.join(
+    os.path.dirname(__file__),
+    'config.json')
 
-config = json.loads(open('config.json').read())
+config = json.loads(open(CONFIG_FILENAME).read())
 list_name = sys.argv[1]
 
 boarddata = requests.get(TRELLO_BOARD_URL.format(
